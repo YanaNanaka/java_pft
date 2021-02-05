55ebeb431051f8df49bf077b0a811186221177cb
YanaNanaka <miss.nanaka@mail.ru>
2021-02-05 21:48:41 +0700

    Добавлено несколько функций (+конспект)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Example.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Example.java
@@ -1,7 +1,27 @@
 package ru.stqa.pft.sandbox;
 
-class Example {
+public class Example {
     public static void main(String args[]) {
-        System.out.println("Простая программа на Java.");
+        program("Java");
+        program("JavaScript");
+        program("C++");
+        double m = 5;
+
+        System.out.println("Площадь квадрата со стороной " + m + " = " + area(m));
+
+        double a = 2;
+        double b = 8;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+
+    }
+    public static void program(String somebody) {
+        System.out.println("Простая программа на " + somebody);
+    }
+    public static double area(double l) {
+        return l * l;
+    }
+    public static double area(double a, double b) {
+        return a * b;
     }
 }
+
