0928486795344dd73d86651dd2ca949b731b035e
YanaNanaka <miss.nanaka@mail.ru>
2021-02-28 21:14:53 +0700

    Тест для класса Point и пр.

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SquareTests {
+    @Test
+    public void testArea() {
+        Square s = new Square(5);
+        Assert.assertEquals(s.area(), 25.0);
+    }
+    @Test
+    public void testDistance() {
+        Point p1 = new Point(1, 5);
+        Point p2 = new Point(4, 9);
+        Assert.assertEquals((p1.distance(p2)), 5.0);
+    }
+}
