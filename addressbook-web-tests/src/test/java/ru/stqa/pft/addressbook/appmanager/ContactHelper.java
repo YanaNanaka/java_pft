401bf1fd0a889b3ee189e8d07b48ca40569812b1
YanaNanaka <miss.nanaka@mail.ru>
2021-03-29 22:16:41 +0700

    6.1 пути к файлам (не работает, т к в приложении не создается контакт с фото)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -30,6 +30,7 @@ public class ContactHelper extends HelperBase {
         type(By.name("home"), contactData.getHome());
         type(By.name("mobile"), contactData.getMobile());
         type(By.name("email"), contactData.getEmail());
+        attach(By.name("photo"), contactData.getPhoto());
 
         if (creation) {
             new Select(wd.findElement(By.name("new_group"))).selectByVisibleText(contactData.getGroup());
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -4,6 +4,8 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.File;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -13,13 +15,24 @@ public class ContactCreationTests extends TestBase {
   public void testContactCreation() {
     Contacts before = app.contact().all();
     app.contact().initContactCreation();
-    ContactData contact = new ContactData().withFirstname("Иван").withMiddlename("Иванович").withLastname("Иванов").
-            withNickname("Ванька").withCompany("КиТ").withAddress("Москва, ул. Лесная, д. 7").withHome("552233").withMobile("89632541789").withGroup("1");
+    File photo = new File("src/test/resources/avatar.png");
+    ContactData contact = new ContactData()
+            .withFirstname("Иван").withMiddlename("Иванович").withLastname("Иванов").
+            withNickname("Ванька").withCompany("КиТ").withAddress("Москва, ул. Лесная, д. 7").
+                    withHome("552233").withMobile("89632541789").withGroup("1").withPhoto(photo);
     app.contact().create(contact, true);
     app.contact().returnToHomePage();
     assertThat(app.contact().сount(), equalTo(before.size() +1));
     Contacts after = app.contact().all();
-    assertThat(after, equalTo(
-            before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+    assertThat(after, equalTo(before.withAdded(contact.
+            withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
+    //@Test
+  //public void testCurrentDir() {
+    //File currentDir = new File(".");
+    //System.out.println(currentDir.getAbsolutePath());
+   // File photo = new File("src/test/resources/avatar.png");
+    //System.out.println(photo.getAbsolutePath());
+   // System.out.println(photo.exists());
+  //}
 }
