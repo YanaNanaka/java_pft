942c7dd5dba56eae6802f93bd54fa1eee3d6c593
YanaNanaka <miss.nanaka@mail.ru>
2021-04-04 19:49:18 +0700

    9.3 (нет проекта)

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -28,6 +28,7 @@ public class ApplicationManager {
     private SessionHelper sessionHelper;
     private DataHelper dataHelper;
     private DbHelper dbHelper;
+    private SoapHelper soapHelper;
 
 
     public ApplicationManager(String browser) {
@@ -113,5 +114,12 @@ public class ApplicationManager {
 
     public DbHelper db() {
         return dbHelper;
+    }
+
+    public SoapHelper soap() {
+        if (soapHelper == null) {
+            soapHelper = new SoapHelper(this);
+        }
+        return soapHelper;
     }
 }
