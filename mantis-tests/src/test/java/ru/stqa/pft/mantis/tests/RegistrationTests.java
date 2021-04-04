8189e76649168cc33ff3ecd22d52cf1fbfc47e50
YanaNanaka <miss.nanaka@mail.ru>
2021-04-04 12:08:46 +0700

    вернула на локальный почтовый сервис

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -12,7 +12,7 @@ import static org.testng.Assert.assertTrue;
 
 public class RegistrationTests extends TestBase {
 
-    //@BeforeMethod
+    @BeforeMethod
     public void startMailServer() {
         app.mail().start();
     }
@@ -23,10 +23,10 @@ public class RegistrationTests extends TestBase {
         String user = String.format("user%s", now);
         String password = "password";
         String email = String.format("user%s@localhost", now);
-        app.james().createUser(user, password);
+        //app.james().createUser(user, password);
         app.registration().start(user, email);
-        //List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
-        List<MailMessage> mailMessages = app.james().waitForMail(user, password, 100000);
+        List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+        //List<MailMessage> mailMessages = app.james().waitForMail(user, password, 100000);
         String confirmationLink = findConfirmationLink(mailMessages, email);
         app.registration().finish(confirmationLink, password);
         assertTrue(app.newSession().login(user, password));
@@ -39,7 +39,7 @@ public class RegistrationTests extends TestBase {
         return regex.getText(mailMessage.text);
     }
 
-    //@AfterMethod (alwaysRun = true)
+    @AfterMethod (alwaysRun = true)
     public void stopMailServer() {
         app.mail().stop();
     }
